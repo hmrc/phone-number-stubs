88694a352dc6e26f0938ea4eb9db964731229695
mar36tin <mar36tin@gmail.com>
2022-09-05 15:31:11 +0100

    Implementation for error handling

--- app/uk/gov/hmrc/cipphonenumberstubs/services/GovNotifyStubService.scala
+++ app/uk/gov/hmrc/cipphonenumberstubs/services/GovNotifyStubService.scala
@@ -21,7 +21,7 @@ import play.api.libs.json.Json
 import play.api.mvc.Result
 import play.api.mvc.Results.{BadRequest, Created, Forbidden, InternalServerError, NotFound, Ok, TooManyRequests}
 import uk.gov.hmrc.cipphonenumberstubs.config.AppConfig
-import uk.gov.hmrc.cipphonenumberstubs.services.responses.{NotificationResponse, VerificationResponses}
+import uk.gov.hmrc.cipphonenumberstubs.services.responses.{NotificationResponses, VerificationResponses}
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
@@ -29,39 +29,39 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton()
 class GovNotifyStubService @Inject() (implicit val executionContext: ExecutionContext, config: AppConfig) extends Logging {
 
-  def sms: Future[Result]  = Future {
-    config.smsErrorCode match {
-      case "badRequestTeamOnlyError" => BadRequest(Json.parse(VerificationResponses.badRequestTeamOnlyError))
-      case "badRequestTrialModeOnlyError" => BadRequest(Json.parse(VerificationResponses.badRequestTrialModeOnlyError))
-      case "authErrorSystemClockError" => Forbidden(Json.parse(VerificationResponses.authErrorSystemClockError))
-      case "authErrorInvalidTokenError" => Forbidden(Json.parse(VerificationResponses.authErrorInvalidTokenError))
-      case "rateLimitError" => TooManyRequests(Json.parse(VerificationResponses.rateLimitError))
-      case "tooManyRequestsError" => TooManyRequests(Json.parse(VerificationResponses.tooManyRequestsError))
-      case "exception" => InternalServerError(Json.parse(VerificationResponses.exception))
+  def sms(phoneNumber: String): Future[Result] = Future {
+    phoneNumber match {
+      case "+447430003001" => BadRequest(Json.parse(VerificationResponses.badRequestTeamOnlyError))
+      case "+447430003002" => BadRequest(Json.parse(VerificationResponses.badRequestTrialModeOnlyError))
+      case "+447430003003" => Forbidden(Json.parse(VerificationResponses.authErrorSystemClockError))
+      case "+447430003004" => Forbidden(Json.parse(VerificationResponses.authErrorInvalidTokenError))
+      case "+447430003005" => TooManyRequests(Json.parse(VerificationResponses.rateLimitError))
+      case "+447430003006" => TooManyRequests(Json.parse(VerificationResponses.tooManyRequestsError))
+      case "+447430003007" => InternalServerError(Json.parse(VerificationResponses.exception))
       case _ => Created(Json.parse(VerificationResponses.notificationResponse))
     }
   }
 
   def status(notificationId: String): Future[Result] = Future {
-    config.notificationErrorCode match {
-      case "validationError" => BadRequest(Json.parse(VerificationResponses.validationError))
-      case "authErrorInvalidTokenError" => Forbidden(Json.parse(VerificationResponses.authErrorInvalidTokenError))
-      case "authErrorSystemClockError" => Forbidden(Json.parse(VerificationResponses.authErrorSystemClockError))
-      case "noResultFoundError" => NotFound(Json.parse(VerificationResponses.noResultFoundError))
+    notificationId match {
+      case "validation-d385-4b17-a0b4-23a85c0c5b1a" => BadRequest(Json.parse(VerificationResponses.validationError))
+      case "invalidtoken-d385-4b17-a0b4-23a85c0c5b1a" => Forbidden(Json.parse(VerificationResponses.authErrorInvalidTokenError))
+      case "systemclock-d385-4b17-a0b4-23a85c0c5b1a" => Forbidden(Json.parse(VerificationResponses.authErrorSystemClockError))
+      case "noresult-d385-4b17-a0b4-23a85c0c5b1a" => NotFound(Json.parse(VerificationResponses.noResultFoundError))
       case _ => Ok(Json.parse(parseNotificationResponse(notificationId)))
     }
   }
 
   def parseNotificationResponse(notificationId: String) = {
     notificationId match {
-      case "16770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponse.permanentFailure
-      case "26770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponse.technicalFailure
-      case "36770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponse.temporaryFailure
-      case "46770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponse.created
-      case "56770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponse.sending
-      case "66770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponse.pending
-      case "76770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponse.sent
-      case _ => NotificationResponse.delivered
+      case "16770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponses.permanentFailure
+      case "26770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponses.technicalFailure
+      case "36770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponses.temporaryFailure
+      case "46770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponses.created
+      case "56770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponses.sending
+      case "66770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponses.pending
+      case "76770ea0-d385-4b17-a0b4-23a85c0c5b1a" => NotificationResponses.sent
+      case _ => NotificationResponses.delivered
     }
   }
 }
