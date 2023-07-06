085e2d22caef8f617665da4bb511c311f0b5e38c
Saqib@Ee <saqib.saleem1@digital.hmrc.gov.uk>
2023-07-06 14:07:37 +0100

    TAV-1070: Fix build + update deps.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object AppDependencies {
 
-  val hmrcBootstrapVersion = "7.2.0"
+  val hmrcBootstrapVersion = "7.19.0"
   val compile = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-28"  % hmrcBootstrapVersion,
   )
   val test = Seq(
-    "uk.gov.hmrc"                   %% "uk.gov.hmrc.bootstrap-test-play-28"  % hmrcBootstrapVersion % Test,
+    "uk.gov.hmrc"                   %% "bootstrap-test-play-28"  % hmrcBootstrapVersion % Test,
   )
 }
